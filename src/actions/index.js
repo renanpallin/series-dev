6c36669a4e994e289a347faf5817682287f93e91
Renan Pallin <renanpallin@gmail.com>
2018-03-13 19:22:38 -0300

    tryLogin action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,55 @@
 import firebase from 'firebase';
+import { Alert } from 'react-native';
 
+const loginSuccess = (user, dispath) =>
+	dispatch({
+		type: 'SET_USER',
+		user,
+	});
 export const tryLogin = (email, password) => async dispatch => {
 	firebase
 		.auth()
 		.signInWithEmailAndPassword(email, password)
 		.then(user => {
 			console.log('LoginPage', user);
-			// this.props.navigation.navigate('Auth', { user });
+			loginSuccess(user, dispatch);
 		})
 		.catch(error => {
-			console.log(error);
-			Alert.alert('ooopts');
-
-			// firebase
-			// 	.auth()
-			// 	.createUserWithEmailAndPassword(email, password)
-			// 	.then(user => loginUserSuccess(dispatch, user))
-			// 	.catch(() => loginUserFail(dispatch));
+			console.log(error.code, error);
+			if (error.code === 'auth/user-not-found') {
+				Alert.alert(
+					'Usuário não encontrado',
+					'Deseja criar um cadastro com as informações inseridas?',
+					[
+						{
+							text: 'Não',
+							onPress: () => {
+								console.log('Cancel Pressed');
+								firebase
+									.auth()
+									.createUserWithEmailAndPassword(
+										email,
+										password
+									)
+									.then(user => loginSuccess(user, dispatch))
+									.catch(error => {
+										Alert.alert(
+											'Oops...',
+											'Um erro ocorreu'
+										);
+									});
+							},
+							style: 'cancel',
+						},
+						{
+							text: 'Sim',
+							onPress: () => console.log('OK Pressed'),
+						},
+					],
+					{ cancelable: false }
+				);
+			} else {
+				Alert.alert('Oops...', 'Um erro ocorreu');
+			}
 		});
-
-
-	dispatch({
-
-	})
-	return ({
-		type: 'TRY_LOGIN',
-		userData,
-	});
 };
